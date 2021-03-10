7a20fd239734ffbb01811e8efddf6371e56ec941
Jean Zhang <jean.nmn.zhang@gmail.com>
2021-03-10 17:35:37 -0500

    removed prismic config file

--- prismic-configuration.js
+++
@@ -1,6 +0,0 @@
-import Prismic from "prismic-javascript";
-
-const REPOSITORY = process.env.PRISMIC_REPOSITORY_NAME;
-const REF_API_URL = `https://${REPOSITORY}.prismic.io/api/v2`;
-export const apiEndpoint = REF_API_URL;
-export const client = Prismic.client(apiEndpoint);
