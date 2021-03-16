bb0d176a1083e14cf5dab9c82ff334acac1fcc26
jzhzhang66 <jzhzhang66@gmail.com>
2021-03-16 11:13:06 -0400

    fixed indentation in return statement and pulled alt text

--- components/AuthorInfo.js
+++ components/AuthorInfo.js
@@ -11,13 +11,17 @@ export default function AuthorInfo({ authorInfo }) {
 
     return (
         <>
-        {renderRichText(authorName)}
-        {renderRichText(cameraInfo)}
-        <img src={signature.url} alt="" />
-        <a>Signature dimensions: {signature.dimensions.width} x {signature.dimensions.height} </a>
-        <br /> <br />
-        <img src={authorImage.url} alt="" />
-        <a>Author image dimensions: {authorImage.dimensions.width} x {authorImage.dimensions.height} </a>
+            {renderRichText(authorName)}
+            {renderRichText(cameraInfo)}
+            <img src={signature.url} alt={signature.alt} />
+            <a>Signature dimensions: {signature.dimensions.width} x {signature.dimensions.height} </a>
+            <br />
+            <a>Signature alt: {signature.alt === null ? "null" : signature.alt}</a>
+            <br />
+            <img src={authorImage.url} alt={authorInfo.alt} />
+            <a>Author image dimensions: {authorImage.dimensions.width} x {authorImage.dimensions.height} </a>
+            <br />
+            <a>Author image alt: {authorImage.alt === null ? "null" : authorImage.alt}</a>
         </>
     )
 }
