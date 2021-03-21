f223d5033b4e5ad03deb927550e8c5c567e97ed1
Jean Zhang <jean.nmn.zhang@gmail.com>
2021-03-21 16:34:15 -0400

    Updated exports to be consistent

--- components/slices/SliceZone.js
+++ components/slices/SliceZone.js
@@ -2,20 +2,20 @@ import React from "react";
 import TextSection from "./TextSection";
 import ImageGallery from "./ImageGallery";
 
-const SliceZone = ({ sliceZone }) => (
-  <>
-    {sliceZone.map((slice, index) => {
-      const { slice_type: sliceType, items, primary } = slice;
-      switch (sliceType) {
-        case "text":
-          return <TextSection {...primary} key={`slice-${index}`} />;
-        case "image_gallery":
-          return <ImageGallery images={items} key={`slice-${index}`} />;
-        default:
-          return null;
-      }
-    })}
-  </>
-);
-
-export default SliceZone;
+export default function SliceZone({ sliceZone }) {
+  return (
+    <>
+      {sliceZone.map((slice, index) => {
+        const { slice_type: sliceType, items, primary } = slice;
+        switch (sliceType) {
+          case "text":
+            return <TextSection {...primary} key={`slice-${index}`} />;
+          case "image_gallery":
+            return <ImageGallery images={items} key={`slice-${index}`} />;
+          default:
+            return null;
+        }
+      })}
+    </>
+  );
+}
