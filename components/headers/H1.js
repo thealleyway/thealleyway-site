2b38385f67e570a1b50d5d7453beff92905b4740
jzhzhang66 <jzhzhang66@gmail.com>
2021-03-23 12:09:23 -0400

    Added some more styling to headers

--- components/headers/H1.js
+++ components/headers/H1.js
@@ -4,6 +4,8 @@ import { getString } from "../../lib/richText";
 
 const H1Title = styled.h1`
     font-size: 80px;
+    font-weight: bold; 
+    font-style: italic; 
 `;
 
 export default function H1({ h1Data }) {
--- components/headers/H2.js
+++ components/headers/H2.js
@@ -4,7 +4,8 @@ import { getString } from "../../lib/richText";
 
 const H2Title = styled.h2`
     font-size: 48px;
-
+    font-weight: bold; 
+    font-style: italic; 
 `;
 
 export default function H2({ h2Data }) {
