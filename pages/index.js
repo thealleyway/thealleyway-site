d43800aec0c65a84c6a74894fc047a58230f53c0
jzhzhang66 <jzhzhang66@gmail.com>
2021-03-30 17:54:30 -0400

    Removed featured stories query code

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,10 @@
-import {
-  getHomePage,
-  getNavigation,
-  getFooter,
-  getFeaturedStories,
-} from '../lib/api';
+import { getHomePage, getNavigation, getFooter } from '../lib/api';
 import Navigation from '../components/navigation/Navigation';
 import Footer from '../components/footer/Footer';
 import { H1, H2, P } from '../style/typography';
 import { getString } from '../lib/richText';
 
-export default function HomePage({
-  homePageData,
-  navigationData,
-  footerData,
-  featuredStoriesData,
-}) {
+export default function HomePage({ homePageData, navigationData, footerData }) {
   const {
     home_page_title: homePageTitle,
     featured_stories_title: featuredStoriesTitle,
@@ -27,13 +17,6 @@ export default function HomePage({
       <Navigation navigationData={navigationData} />
       <H1>{getString(homePageTitle)}</H1>
       <H2>{getString(featuredStoriesTitle)}</H2>
-      {featuredStoriesData.map((item) => (
-        <div key={item.id}>
-          <H2>{getString(item.storyTitle)}</H2>
-          <P>{item.storyDate}</P>
-          <img src={item.previewImage.url} alt={item.previewImage.alt} />
-        </div>
-      ))}
       <H2>{getString(newsletterSignup)}</H2>
       <P>{getString(newsletterDescription)}</P>
       <Footer footerData={footerData} />
@@ -45,16 +28,12 @@ export async function getStaticProps() {
   const homePageData = await getHomePage();
   const footerData = await getFooter();
   const navigationData = await getNavigation();
-  const featuredStoriesIds = homePageData.featured_stories.map((item) => {
-    return item.story.id;
-  });
-  const featuredStoriesData = await getFeaturedStories(featuredStoriesIds);
+
   return {
     props: {
       homePageData,
       footerData,
       navigationData,
-      featuredStoriesData,
     },
   };
 }
