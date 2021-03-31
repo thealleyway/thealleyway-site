a6c8bfb857d670740e9591092406d7cb73992426
Izzy Berzsenyi <iberzsenyi@yahoo.com>
2021-03-30 21:34:32 -0400

    Creating a contact confirmation component

---
+++ components/ContactConfirmation.js
@@ -0,0 +1,19 @@
+import { renderRichText } from '../lib/richText';
+
+export default function ContactConfirmation({ contactConfirmationData }) {
+  const {
+    contact_confirmation_title_regular_1: contactConfirmationTitleRegular1,
+    contact_confirmation_title_italic_2: contactConfirmationTitleItalic2,
+    contact_confirmation_title_regular_3: contactConfirmationTitleRegular3,
+    contact_confirmation_description: description,
+  } = contactConfirmationData;
+
+  return (
+    <>
+      {renderRichText(contactConfirmationTitleRegular1)}
+      {renderRichText(contactConfirmationTitleItalic2)}
+      {renderRichText(contactConfirmationTitleRegular3)}
+      {renderRichText(description)}
+    </>
+  );
+}
