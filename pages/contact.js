1653f78c0f179f481e37533ce6bfa4cfd178bd8b
Izzy Berzsenyi <iberzsenyi@yahoo.com>
2021-03-30 21:34:51 -0400

    Update Contact page to include prettier styling, and updated prismic data changes

--- pages/contact.js
+++ pages/contact.js
@@ -1,6 +1,7 @@
 import { getContactPage, getNavigation, getFooter } from '../lib/api';
 import Navigation from '../components/navigation/Navigation';
 import Footer from '../components/footer/Footer';
+import ContactConfirmation from '../components/ContactConfirmation';
 import { H1, P } from '../style/typography';
 import { getString } from '../lib/richText';
 
@@ -10,15 +11,29 @@ export default function ContactPage({
   footerData,
 }) {
   const {
-    contact_page_title: contactPageTitle,
+    contact_page_title_regular: contactPageTitleRegular,
+    contact_page_title_italic: contactPageTitleItalic,
     contact_page_description: contactPageDescription,
+    contact_confirmation_title_regular_1,
+    contact_confirmation_title_italic_2,
+    contact_confirmation_title_regular_3,
+    contact_confirmation_description,
   } = contactPageData;
+
+  const contactConfirmationData = {
+    contact_confirmation_title_regular_1,
+    contact_confirmation_title_italic_2,
+    contact_confirmation_title_regular_3,
+    contact_confirmation_description,
+  };
 
   return (
     <>
       <Navigation navigationData={navigationData} />
-      <H1>{getString(contactPageTitle)}</H1>
+      <H1>{getString(contactPageTitleRegular)}</H1>
+      <H1>{getString(contactPageTitleItalic)}</H1>
       <P>{getString(contactPageDescription)}</P>
+      <ContactConfirmation contactConfirmationData={contactConfirmationData} />
       <Footer footerData={footerData} />
     </>
   );
