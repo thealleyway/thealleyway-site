19da8ad68e878d803b744af9c9710fe06faa75b5
Izzy Berzsenyi <iberzsenyi@yahoo.com>
2021-04-01 13:59:40 -0400

    Create Newsletter Confirmation component that renders Prismic data

---
+++ components/newsletterConfirmation/NewsletterConfirmation.js
@@ -0,0 +1,19 @@
+import { H1, H3 } from '../../style/typography';
+import { getString } from '../../lib/richText';
+
+export default function NewsletterConfirmation({ newsletterConfirmationData }) {
+  const {
+    newsletter_confirmation_title_1: title1,
+    newsletter_confirmation_title_2: title2,
+    newsletter_confirmation_description: description,
+    newsletter_confirmation_image: image,
+  } = newsletterConfirmationData;
+  return (
+    <>
+      <H1>{getString(title1)}</H1>
+      <H1>{getString(title2)}</H1>
+      <H3>{getString(description)}</H3>
+      <img src={image.url} alt={image.alt} />
+    </>
+  );
+}
