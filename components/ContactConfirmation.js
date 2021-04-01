2b1519dd5397aa17900f38b4e39ff43e7d111dc2
Izzy Berzsenyi <iberzsenyi@yahoo.com>
2021-04-01 13:30:19 -0400

    Pulling images from Prismic to Contact and Confirmation page

--- components/ContactConfirmation.js
+++ components/ContactConfirmation.js
@@ -6,6 +6,7 @@ export default function ContactConfirmation({ contactConfirmationData }) {
     contact_confirmation_title_italic_2: contactConfirmationTitleItalic2,
     contact_confirmation_title_regular_3: contactConfirmationTitleRegular3,
     contact_confirmation_description: description,
+    contact_confirmation_image: image,
   } = contactConfirmationData;
 
   return (
@@ -14,6 +15,7 @@ export default function ContactConfirmation({ contactConfirmationData }) {
       {renderRichText(contactConfirmationTitleItalic2)}
       {renderRichText(contactConfirmationTitleRegular3)}
       {renderRichText(description)}
+      <img src={image.url} alt={image.alt} />
     </>
   );
 }
--- pages/contact.js
+++ pages/contact.js
@@ -14,10 +14,13 @@ export default function ContactPage({
     contact_page_title_regular: contactPageTitleRegular,
     contact_page_title_italic: contactPageTitleItalic,
     contact_page_description: contactPageDescription,
+    contact_image_top: contactImageTop,
+    contact_image_bottom: contactImageBottom,
     contact_confirmation_title_regular_1,
     contact_confirmation_title_italic_2,
     contact_confirmation_title_regular_3,
     contact_confirmation_description,
+    contact_confirmation_image,
   } = contactPageData;
 
   const contactConfirmationData = {
@@ -25,6 +28,7 @@ export default function ContactPage({
     contact_confirmation_title_italic_2,
     contact_confirmation_title_regular_3,
     contact_confirmation_description,
+    contact_confirmation_image,
   };
 
   return (
@@ -33,6 +37,8 @@ export default function ContactPage({
       <H1>{getString(contactPageTitleRegular)}</H1>
       <H1>{getString(contactPageTitleItalic)}</H1>
       <P>{getString(contactPageDescription)}</P>
+      <img src={contactImageTop.url} alt={contactImageTop.alt} />
+      <img src={contactImageBottom.url} alt={contactImageBottom.alt} />
       <ContactConfirmation contactConfirmationData={contactConfirmationData} />
       <Footer footerData={footerData} />
     </>
