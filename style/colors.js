d0f05068434b29eb1d375e1de8bd9ec5904a31e5
Izzy Berzsenyi <iberzsenyi@yahoo.com>
2021-04-01 14:28:38 -0400

    Importing all the colors into a colors.js file

---
+++ style/colors.js
@@ -0,0 +1,12 @@
+const colors = {
+  CREME: '#E9E3DA',
+  BURNTORANGE: '#BD7045',
+  BROWN: '#5F3117',
+  OLIVE: '#AAA378',
+  SANDYYELLOW: '#D6B482',
+  WHITE: '#FFFFFF',
+  BLACK: '#333333',
+  MAUVE: '#B27C7C',
+};
+
+export { colors };
