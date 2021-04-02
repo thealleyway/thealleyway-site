2336ddc35586a7771015d0fdf97706bd1d29459d
Jean Zhang <jean.nmn.zhang@gmail.com>
2021-04-02 16:58:34 -0400

    Updated to Swear Display and Poppins

--- style/typography.js
+++ style/typography.js
@@ -1,52 +1,51 @@
-import styled from 'styled-components'
+import styled from 'styled-components';
 const fonts = {
-    cormorant: `'cormorant-garamond', serif`,
-    aktiv: `'aktiv-grotesk', sans-serif`
-}
-
+  swearDisplay: 'swear-display, serif',
+  poppins: 'poppins, sans-serif',
+};
 
 const fontSizes = {
-    h1: `clamp(2.5em, 8vw, 8em)`,
-    h2: `clamp(1.8em, 5vw, 6em)`,
-    h3: `clamp(1em, 2.5vw, 3em)`,
-    p: `clamp(.6em, 1.5vw, 2em)`,
-}
+  h1: 'clamp(2.5em, 8vw, 8em)',
+  h2: 'clamp(1.8em, 5vw, 6em)',
+  h3: 'clamp(1em, 2.5vw, 3em)',
+  p: 'clamp(.6em, 1.5vw, 2em)',
+};
 
 const fontStyles = {
-    italic: 'italic',
-}
+  italic: 'italic',
+};
 
 const fontWeights = {
-    normal: `400px`,
-    medium: `700px`,
-    bold: `900px`,
-}
+  normal: `400px`,
+  medium: `700px`,
+  bold: `900px`,
+};
 
 const H1 = styled.h1`
-    font-size: ${fontSizes.h1};
-    font-family: ${fonts.cormorant}; 
-    font-style: ${fontStyles.italic}; 
-    font-weight: ${fontWeights.bold};
+  font-size: ${fontSizes.h1};
+  font-family: ${fonts.swearDisplay};
+  font-style: ${fontStyles.italic};
+  font-weight: ${fontWeights.bold};
 `;
 
 const H2 = styled.h2`
-    font-size: ${fontSizes.h2};
-    font-family: ${fonts.cormorant}; 
-    font-style: ${fontStyles.italic}; 
-    font-weight: ${fontWeights.bold};
+  font-size: ${fontSizes.h2};
+  font-family: ${fonts.swearDisplay};
+  font-style: ${fontStyles.italic};
+  font-weight: ${fontWeights.bold};
 `;
 
 const H3 = styled.h3`
-    font-size: ${fontSizes.h3};
-    font-family: ${fonts.aktiv}; 
-    font-style: ${fontStyles.italic}; 
-    font-weight: ${fontWeights.medium};
+  font-size: ${fontSizes.h3};
+  font-family: ${fonts.poppins};
+  font-style: ${fontStyles.italic};
+  font-weight: ${fontWeights.medium};
 `;
 
 const P = styled.p`
-    font-size: ${fontSizes.p};
-    font-family: ${fonts.aktiv}; 
-    font-weight: ${fontWeights.regular};
+  font-size: ${fontSizes.p};
+  font-family: ${fonts.poppins};
+  font-weight: ${fontWeights.regular};
 `;
 
-export { H1, H2, H3, P}
+export { H1, H2, H3, P };
