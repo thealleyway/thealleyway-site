da94d8c939f5b6d43877aefb15b474d1beabd3db
Izzy Berzsenyi <iberzsenyi@yahoo.com>
2021-04-07 18:28:25 -0400

    Adding for attribute in label, and for in input

--- components/textInputField/TextInputField.js
+++ components/textInputField/TextInputField.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { TextInput, TextLabel } from './TextInputField.style';
 
-export default function TextInputField({ label, onChange }) {
+export default function TextInputField({ id, label, onChange }) {
   return (
     <>
-      <TextLabel>{label}</TextLabel>
-      <TextInput type="text" onChange={(e) => onChange(e.target.value)} />
+      <TextLabel for={id}>{label}</TextLabel>
+      <TextInput
+        type="text"
+        name={id}
+        onChange={(e) => onChange(e.target.value)}
+      />
     </>
   );
 }
