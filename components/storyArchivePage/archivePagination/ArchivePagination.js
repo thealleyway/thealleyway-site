d71e04184e8851a5cb006d0f8a37bd510db21a0a
Izzy Berzsenyi <iberzsenyi@yahoo.com>
2021-04-15 22:58:06 -0400

    ArchivePagination component and styling

---
+++ components/storyArchivePage/archivePagination/ArchivePagination.js
@@ -0,0 +1,33 @@
+import PageLink from '../../pageLink/PageLink';
+import { ArchiveArrow, PaginationWrapper } from './ArchivePagination.style';
+import { P } from '../../../style/typography';
+import { icons } from '../../../style/icons';
+
+export default function ArchivePagination({ paginationData, sortType }) {
+  const { totalPages, page } = paginationData;
+  const prevDisabled = page == 1;
+  const nextDisabled = page == totalPages;
+  const ARCHIVE = `/archive/${sortType.toLowerCase()}/`;
+
+  return (
+    <PaginationWrapper>
+      <PageLink href={ARCHIVE + (page - 1)}>
+        <ArchiveArrow
+          src={icons.FILLED_SHORT_ARROW}
+          disabled={prevDisabled}
+          previous={true}
+        />
+      </PageLink>
+      <P>
+        {page} / {totalPages}
+      </P>
+      <PageLink href={ARCHIVE + (page + 1)}>
+        <ArchiveArrow
+          src={icons.FILLED_SHORT_ARROW}
+          disabled={nextDisabled}
+          previous={false}
+        />
+      </PageLink>
+    </PaginationWrapper>
+  );
+}
---
+++ components/storyArchivePage/archivePagination/ArchivePagination.style.js
@@ -0,0 +1,35 @@
+import styled from 'styled-components';
+import { max } from '../../../lib/responsive';
+
+const ArchiveArrow = styled.img`
+  width: 2em;
+  pointer-events: ${(props) => (props.disabled ? 'none' : 'auto')};
+  opacity: ${(props) => (props.disabled ? '0.50' : '1')};
+  transform: ${(props) =>
+    props.previous ? 'rotate(-90deg)' : 'rotate(90deg)'};
+
+  &:hover {
+    cursor: pointer;
+    opacity: 0.5;
+  }
+
+  @media only screen and ${max.tabletLg} {
+    width: 2.5em;
+  }
+
+  @media only screen and ${max.tabletSm} {
+    width: 2em;
+  }
+`;
+
+const PaginationWrapper = styled.div`
+  width: 20em;
+  right: 0;
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-end;
+  align-items: center;
+  margin-right: -5px;
+`;
+
+export { ArchiveArrow, PaginationWrapper };
