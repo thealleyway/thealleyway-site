94b4f802414891969792ef9ef4d087f19f3aa898
Izzy Berzsenyi <iberzsenyi@yahoo.com>
2021-04-15 22:58:20 -0400

    ArchiveSorting component and styling

---
+++ components/storyArchivePage/archiveSorting/ArchiveSorting.style.js
@@ -0,0 +1,34 @@
+import styled from 'styled-components';
+import { P, fontWeights } from '../../../style/typography';
+import { colors } from '../../../style/colors';
+
+const SortLabelWrapper = styled.div`
+  overflow: hidden;
+  display: flex;
+  flex-direction: column;
+`;
+
+const SortLabel = styled(P)`
+  color: ${colors.BROWN};
+  font-weight: ${(props) =>
+    props.enabled ? fontWeights.bold : fontWeights.normal};
+  letter-spacing: 0.25px;
+  margin: 0;
+
+  ${SortLabelWrapper}:hover & {
+    font-weight: ${fontWeights.bold};
+    cursor: pointer;
+  }
+`;
+
+const SortUnderline = styled.img`
+  visibility: ${(props) => (props.enabled ? 'visible' : 'hidden')};
+  transform: ${(props) => (props.alpha ? 'scale(4)' : 'scale(1)')};
+  margin-top: ${(props) => (props.alpha ? '0' : '-5px')};
+
+  ${SortLabelWrapper}:hover & {
+    visibility: visible;
+  }
+`;
+
+export { SortLabelWrapper, SortLabel, SortUnderline };
