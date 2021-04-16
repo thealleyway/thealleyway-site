a1ad5213767ed66e20ebf37b84c4e0e0c53f37c2
Izzy Berzsenyi <iberzsenyi@yahoo.com>
2021-04-15 23:01:45 -0400

    StoryArchivePage component and styling

---
+++ components/storyArchivePage/StoryArchivePage.js
@@ -0,0 +1,76 @@
+import React from 'react';
+import Navigation from '../navigation/Navigation';
+import Footer from '../footer/Footer';
+import ArchiveStoryPreview from './archiveStoryPreview/ArchiveStoryPreview';
+import ArchivePagination from './archivePagination/ArchivePagination';
+import ArchiveSorting from './archiveSorting/ArchiveSorting';
+import {
+  ArchiveContainer,
+  ArchiveSortingWrapper,
+  StoriesWrapper,
+  SortingPaginationWrapper,
+  FooterPaginationWrapper,
+} from './StoryArchivePage.style';
+import H1Styled from '../H1Styled/H1Styled';
+import PageDescription from '../pageDescription/PageDescription';
+
+const ALPHA = 'alpha';
+const DATE = 'date';
+
+export default function StoryArchivePage({
+  storyArchivePageData,
+  storiesData,
+  sortingData,
+  authorSignatures,
+  navigationData,
+  footerData,
+  paginationData,
+}) {
+  const {
+    archive_title_regular: archiveTitleRegular,
+    archive_title_italic: archiveTitleItalic,
+    archive_description: archiveDescription,
+    archive_header_image: archiveHeaderImage,
+  } = storyArchivePageData;
+
+  const sortType = sortingData == DATE.toLowerCase() ? DATE : ALPHA;
+
+  const StoryPreviews = storiesData.map((story) => {
+    const signature = authorSignatures[story.author_info_id];
+    return (
+      <ArchiveStoryPreview key={story.id} story={story} signature={signature} />
+    );
+  });
+
+  return (
+    <>
+      <Navigation navigationData={navigationData} />
+      <H1Styled regular={archiveTitleRegular} italicized={archiveTitleItalic} />
+      <PageDescription
+        description={archiveDescription}
+        arrowText="EXPLORE THE ARCHIVE"
+        img={archiveHeaderImage}
+      />
+      <ArchiveContainer>
+        <SortingPaginationWrapper>
+          <ArchiveSortingWrapper>
+            <ArchiveSorting order={DATE} sortType={sortType} />
+            <ArchiveSorting order={ALPHA} sortType={sortType} />
+          </ArchiveSortingWrapper>
+          <ArchivePagination
+            paginationData={paginationData}
+            sortType={sortType}
+          />
+        </SortingPaginationWrapper>
+        <StoriesWrapper>{StoryPreviews}</StoriesWrapper>
+        <FooterPaginationWrapper>
+          <ArchivePagination
+            paginationData={paginationData}
+            sortType={sortType}
+          />
+        </FooterPaginationWrapper>
+      </ArchiveContainer>
+      <Footer footerData={footerData} />
+    </>
+  );
+}
---
+++ components/storyArchivePage/StoryArchivePage.style.js
@@ -0,0 +1,81 @@
+import styled from 'styled-components';
+import { max } from '../../lib/responsive';
+
+const STORY_SPACING = 48;
+
+const ArchiveContainer = styled.div`
+  padding-top: clamp(5em, 10vw, 6em);
+`;
+
+const ArchiveSortingWrapper = styled.div`
+  width: clamp(12em, 25vw, 16em);
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  margin-bottom: 0.5em;
+
+  @media only screen and ${max.tabletLg} {
+    width: clamp(18em, 100vw, 20em);
+  }
+
+  @media only screen and ${max.mobile} {
+    width: 26em;
+  }
+`;
+
+const SortingPaginationWrapper = styled.div`
+  flex: 100%;
+  display: flex;
+  flex-direction: row;
+  max-width: 70em;
+  margin: 0 auto;
+  justify-content: space-between;
+  align-items: flex-end;
+
+  @media only screen and ${max.desktop} {
+    max-width: 65em;
+  }
+
+  @media only screen and ${max.tabletLg} {
+    margin: 0 calc(${STORY_SPACING}px * 1.3);
+  }
+
+  @media only screen and ${max.tabletSm} {
+    margin: 0
+      clamp(
+        calc(${STORY_SPACING}px * 0.25),
+        3vw,
+        calc(${STORY_SPACING}px * 0.5)
+      );
+  }
+`;
+
+const StoriesWrapper = styled.div`
+  flex: 100%;
+  display: flex;
+  flex-wrap: wrap;
+  position: relative;
+  justify-content: center;
+`;
+
+const FooterPaginationWrapper = styled(SortingPaginationWrapper)`
+  justify-content: flex-end;
+  margin-top: calc(${STORY_SPACING}px * -1.5);
+
+  @media only screen and ${max.tabletLg} {
+    margin-top: calc(${STORY_SPACING}px * -1);
+  }
+
+  @media only screen and ${max.tabletSm} {
+    margin-top: calc(${STORY_SPACING}px * -0.25);
+  }
+`;
+
+export {
+  ArchiveContainer,
+  ArchiveSortingWrapper,
+  StoriesWrapper,
+  SortingPaginationWrapper,
+  FooterPaginationWrapper,
+  STORY_SPACING,
+};
