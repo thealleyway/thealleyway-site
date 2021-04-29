517e22ffcd996efdb55d165b10e2b57b0cdf2542
jzhzhang66 <59738880+jzhzhang66@users.noreply.github.com>
2021-04-29 11:02:32 -0400

    Added logic for required field to input field

--- components/text-input-field/TextInputField.style.js
+++ components/text-input-field/TextInputField.style.js
@@ -6,6 +6,15 @@ const TextLabel = styled(P)`
   color: ${colors.BROWN};
   display: block;
   margin: 0;
+  padding-left: ${(props) => (props.required ? 0 : `2em`)};
+`;
+
+const StarContainer = styled.div`
+  display: flex;
+`;
+
+const RedStar = styled.img`
+  padding-right: 1.3em;
 `;
 
 const TextInput = styled(P).attrs({
@@ -18,9 +27,10 @@ const TextInput = styled(P).attrs({
   width: 100%;
   border-radius: 0%;
 
+  margin-left: 2.2em;
   &:focus {
     outline: none;
   }
 `;
 
-export { TextLabel, TextInput };
+export { TextLabel, TextInput, RedStar, StarContainer };
