3f402d8f8cd96a021ff1cc28021123950c7c6e47
Izzy Berzsenyi <iberzsenyi@yahoo.com>
2021-05-02 13:00:08 -0500

    Move main resource component into about-page folder

--- components/main-resource/MainResource.js
+++
@@ -1,7 +0,0 @@
-import { getString } from '../../lib/richText';
-import { H3 } from '../../style/typography';
-export default function MainResource({ mainResourceData }) {
-  const { resourceTitle } = mainResourceData;
-
-  return <H3>{getString(resourceTitle)}</H3>;
-}
