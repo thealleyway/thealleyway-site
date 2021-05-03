4e505932a6a7ba500bde058f90a8a19811f337ec
jzhzhang66 <59738880+jzhzhang66@users.noreply.github.com>
2021-05-02 20:46:12 -0400

    Compiled!

---
+++ components/more-about-popup/MoreAboutPopup.styles.js
@@ -0,0 +1,86 @@
+import styled from 'styled-components';
+import { min } from '../../lib/responsive';
+import { colors } from '../../style/colors';
+import {H4, fontWeights} from '../../style/typography';
+
+const PopupTitle = styled(H4)`
+    color: ${colors.MAUVE};
+`;
+
+const TextContentWrapper = styled.div`
+    width: 80%;
+    margin: 2.1em 0 0 1.3em;
+`;
+
+const PopupImage = styled.img`
+opacity: 10%;
+object-fit: cover;
+position: absolute;
+top: 0;
+right: 0;
+width: 100%;
+height: 100%;
+`;
+
+const CloseIcon = styled.img`
+  position: absolute;
+  border: none;
+  top: 1em;
+  right: 1em;
+  width: clamp(1.3em, 7vw, 1.6em);
+  @media ${min.desktop} {
+    width: 1.3em;
+  }
+`;
+
+const PopupBox = styled.div`
+  border: none;
+  position: fixed;
+  width: 100%;
+  height: 100vh;
+  z-index: 5;
+  font-size: .9em;
+  top: 20%;
+  @media ${min.tablet} {
+    top: 30%;
+  }
+  @media ${min.desktop} {
+    top: 15%;
+    left: 1em;
+  }
+  @media ${min.desktopLg} {
+    top: 0;
+  }
+`;
+
+const Box = styled.div`
+  background: ${colors.CREME};
+  position: relative;
+  margin: 0 auto;
+  border: 0.15em solid ${colors.BLACK};
+  overflow: auto;
+  height: 25em
+  padding: 1em;
+  width: 80%;
+  top: 0;
+  @media ${min.tablet} {
+    height: 29em;
+  }
+  @media ${min.desktop} {
+    height: 25em;
+    padding: 2em;
+    width: 29%;
+  }
+  @media ${min.desktopLg} {
+    top: 22%;
+  }
+`;
+
+export {
+    PopupBox,
+    Box,
+    PopupTitle,
+    TextContentWrapper,
+    PopupImage,
+    CloseIcon,
+}
