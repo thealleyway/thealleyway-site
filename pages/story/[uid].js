cf27135cb29c32c622db4e47d44c59b26d8a9de0
Jean Zhang <jean.nmn.zhang@gmail.com>
2021-05-04 02:01:27 -0400

    Removed unused variables

--- pages/story/[uid].js
+++ pages/story/[uid].js
@@ -1,26 +1,8 @@
-import {
-  getAuthorInfo,
-  getStories,
-  getStory,
-  getNavigation,
-  getFooter,
-} from '../../lib/api';
+import { getAuthorInfo, getStories, getStory } from '../../lib/api';
 import StoryPage from '../../components/story-page/StoryPage';
 
-export default function Story({
-  storyData,
-  authorInfo,
-  navigationData,
-  footerData,
-}) {
-  return (
-    <StoryPage
-      storyData={storyData}
-      authorInfo={authorInfo}
-      navigationData={navigationData}
-      footerData={footerData}
-    />
-  );
+export default function Story({ storyData, authorInfo }) {
+  return <StoryPage storyData={storyData} authorInfo={authorInfo} />;
 }
 
 export async function getStaticPaths() {
@@ -39,15 +21,11 @@ export async function getStaticPaths() {
 export async function getStaticProps({ params }) {
   const storyData = await getStory(params.uid);
   const authorInfo = await getAuthorInfo(storyData.author_info.id);
-  const footerData = await getFooter();
-  const navigationData = await getNavigation();
 
   return {
     props: {
       storyData,
       authorInfo,
-      footerData,
-      navigationData,
     },
   };
 }
