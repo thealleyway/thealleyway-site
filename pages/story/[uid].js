eb7438df7176cee9ce78f46d7d04df3ab53514f6
Jean Zhang <jean.nmn.zhang@gmail.com>
2021-05-05 03:43:18 -0400

    Grab navigation and footer data

--- pages/story/[uid].js
+++ pages/story/[uid].js
@@ -1,8 +1,26 @@
-import { getAuthorInfo, getStories, getStory } from '../../lib/api';
+import {
+  getAuthorInfo,
+  getStories,
+  getStory,
+  getNavigation,
+  getFooter,
+} from '../../lib/api';
 import StoryPage from '../../components/story-page/StoryPage';
 
-export default function Story({ storyData, authorInfo }) {
-  return <StoryPage storyData={storyData} authorInfo={authorInfo} />;
+export default function Story({
+  storyData,
+  authorInfo,
+  navigationData,
+  footerData,
+}) {
+  return (
+    <StoryPage
+      storyData={storyData}
+      authorInfo={authorInfo}
+      navigationData={navigationData}
+      footerData={footerData}
+    />
+  );
 }
 
 export async function getStaticPaths() {
@@ -21,11 +39,15 @@ export async function getStaticPaths() {
 export async function getStaticProps({ params }) {
   const storyData = await getStory(params.uid);
   const authorInfo = await getAuthorInfo(storyData.author_info.id);
+  const navigationData = await getNavigation();
+  const footerData = await getFooter();
 
   return {
     props: {
       storyData,
       authorInfo,
+      navigationData,
+      footerData,
     },
   };
 }
