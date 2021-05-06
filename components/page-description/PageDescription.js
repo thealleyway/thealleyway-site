94072487ee83d5f010347d9e99c6e9d3be23e809
jzhzhang66 <59738880+jzhzhang66@users.noreply.github.com>
2021-05-06 16:27:50 -0400

    Contact page description animations done

--- components/page-description/PageDescription.js
+++ components/page-description/PageDescription.js
@@ -6,6 +6,7 @@ import {
   Img,
 } from './PageDescription.styles';
 import LongSparkArrow from '../long-spark-arrow/LongSparkArrow';
+import { useSpring } from 'react-spring';
 
 export default function PageDescription({
   description,
@@ -13,15 +14,28 @@ export default function PageDescription({
   img,
   scrollTo,
 }) {
+  const descriptionAnimation = useSpring({
+    to: { opacity: 1, transform: 'translateX(0)' },
+    from: { opacity: 0, transform: 'translateX(-10em)' },
+    config: { tension: 20, friction: 15 },
+    delay: 1500,
+  });
+
+  const imgAnimation = useSpring({
+    to: { opacity: 0.1, transform: 'translateX(0)' },
+    from: { opacity: 0, transform: 'translateX(10em)' },
+    config: { tension: 20, friction: 15 },
+  });
+
   return (
     <div>
-      <PageDescriptionContainer>
+      <PageDescriptionContainer style={descriptionAnimation}>
         <LongSparkArrow arrowText={arrowText} scrollTo={scrollTo} />
         <DescriptionContainer>
           <Description>{getString(description)}</Description>
         </DescriptionContainer>
       </PageDescriptionContainer>
-      <Img src={img.url} alt={img.alt} />
+      <Img src={img.url} alt={img.alt} style={imgAnimation} />
     </div>
   );
 }
