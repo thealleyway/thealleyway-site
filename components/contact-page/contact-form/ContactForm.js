cd71af42e5573bec1d2dd411783aa9e255eb2b7a
jzhzhang66 <59738880+jzhzhang66@users.noreply.github.com>
2021-05-11 00:18:47 -0400

    Added fade animation for contact page

--- components/contact-page/contact-form/ContactForm.js
+++ components/contact-page/contact-form/ContactForm.js
@@ -6,9 +6,11 @@ import {
   InputBoxWrapper,
   ButtonWrapper,
 } from './ContactForm.styles';
-import React, { useState } from 'react';
 import { emailEndpoint, axiosConfig, proxyurl } from '../../../lib/utils';
 import { fieldNames } from '../../../lib/utils';
+import React, { useState, useRef, useEffect } from "react";
+import { registerObserver } from '../../../lib/intersectionObserver';
+import { PlaceHolder } from '../../base-components/BaseComponents';
 
 const axios = require('axios');
 
@@ -44,66 +46,79 @@ export default function ContactForm({ togglePopup }) {
     return formIsValid;
   };
 
-  return (
-    <ContactFormContainer>
-      <TextInputField
-        id="firstName"
-        label="First Name"
-        required
-        showError={errors[fieldNames.FIRST_NAME]}
-        value={
-          fields[fieldNames.FIRST_NAME] ? fields[fieldNames.FIRST_NAME] : ''
-        }
-        onChange={(e) => setFields({ ...fields, FIRST_NAME: e.target.value })}
-      />
-      <TextInputField
-        id="lastName"
-        label="Last Name"
-        value={fields[fieldNames.LAST_NAME] ? fields[fieldNames.LAST_NAME] : ''}
-        onChange={(e) => setFields({ ...fields, LAST_NAME: e.target.value })}
-      />
-      <TextInputField
-        id="email"
-        label="Email"
-        required
-        showError={errors[fieldNames.EMAIL]}
-        value={fields[fieldNames.EMAIL] ? fields[fieldNames.EMAIL] : ''}
-        onChange={(e) => setFields({ ...fields, EMAIL: e.target.value })}
-      />
-      <InputBoxWrapper>
-        <TextInputBox
-          label="Message"
-          showError={errors[fieldNames.MESSAGE]}
+  const placeHolderRef = useRef(null);
+  const [visible, setVisible] = useState(false);
+
+  useEffect(() => {
+    registerObserver(placeHolderRef.current, setVisible);
+  }, []);
+
+  if (visible) {
+    return (
+      <ContactFormContainer
+
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ type: "spring", duration: 4 }}
+      >
+        <TextInputField
+          id="firstName"
+          label="First Name"
           required
-          value={fields[fieldNames.MESSAGE] ? fields[fieldNames.MESSAGE] : ''}
-          onChange={(e) => setFields({ ...fields, MESSAGE: e.target.value })}
+          showError={errors[fieldNames.FIRST_NAME]}
+          value={
+            fields[fieldNames.FIRST_NAME] ? fields[fieldNames.FIRST_NAME] : ''
+          }
+          onChange={(e) => setFields({ ...fields, FIRST_NAME: e.target.value })}
         />
-      </InputBoxWrapper>
-      <ButtonWrapper>
-        <SquareButton
-          aria-label="Submission Button"
-          buttonText="SUBMIT"
-          onClick={() => {
-            if (submitRequest()) {
-              document.getElementById('area').value = '';
-              setFields({});
-              togglePopup();
-            }
-          }}
+        <TextInputField
+          id="lastName"
+          label="Last Name"
+          value={fields[fieldNames.LAST_NAME] ? fields[fieldNames.LAST_NAME] : ''}
+          onChange={(e) => setFields({ ...fields, LAST_NAME: e.target.value })}
         />
-      </ButtonWrapper>
-    </ContactFormContainer>
-  );
+        <TextInputField
+          id="email"
+          label="Email"
+          required
+          showError={errors[fieldNames.EMAIL]}
+          value={fields[fieldNames.EMAIL] ? fields[fieldNames.EMAIL] : ''}
+          onChange={(e) => setFields({ ...fields, EMAIL: e.target.value })}
+        />
+        <InputBoxWrapper>
+          <TextInputBox
+            label="Message"
+            showError={errors[fieldNames.MESSAGE]}
+            required
+            value={fields[fieldNames.MESSAGE] ? fields[fieldNames.MESSAGE] : ''}
+            onChange={(e) => setFields({ ...fields, MESSAGE: e.target.value })}
+          />
+        </InputBoxWrapper>
+        <ButtonWrapper>
+          <SquareButton
+            aria-label="Submission Button"
+            buttonText="SUBMIT"
+            onClick={() => {
+              if (submitRequest()) {
+                document.getElementById('area').value = '';
+                setFields({});
+                togglePopup();
+              }
+            }}
+          />
+        </ButtonWrapper>
+      </ContactFormContainer>
+    );
+  }
+  return <PlaceHolder ref={placeHolderRef} />;
 
   function submitRequest() {
-    const name = `${fields[fieldNames.FIRST_NAME]} ${
-      fields[fieldNames.LAST_NAME]
-    }`;
+    const name = `${fields[fieldNames.FIRST_NAME]} ${fields[fieldNames.LAST_NAME]
+      }`;
     const subject = `Get in touch - ${name}`;
     if (isValidSubmission()) {
-      const request = `${emailEndpoint}?name=${fields['name']}&email=${
-        fields[fieldNames.EMAIL]
-      }&subject=${subject}&body=${fields[fieldNames.MESSAGE]}`;
+      const request = `${emailEndpoint}?name=${fields['name']}&email=${fields[fieldNames.EMAIL]
+        }&subject=${subject}&body=${fields[fieldNames.MESSAGE]}`;
       axios
         .post(proxyurl + request, axiosConfig)
         .then((response) => {
