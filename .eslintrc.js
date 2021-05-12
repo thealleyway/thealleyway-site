2a922656a40f7233b2ad176ad7ed81c0e390633c
jzhzhang66 <59738880+jzhzhang66@users.noreply.github.com>
2021-05-12 08:59:33 -0400

    reset eslint file

--- .eslintrc.js
+++ .eslintrc.js
@@ -38,13 +38,5 @@ module.exports = {
     'unicorn/prevent-abbreviations': 'off',
     'unicorn/no-null': 'off',
     'unicorn/no-nested-ternary': 'off',
-    'sonarjs/cognitive-complexity': 'off',
-    'security/detect-object-injection': 'off',
-    'react-hooks/rules-of-hooks': 'off',
-    'sonarjs/no-duplicate-string': 'off',
-    'unicorn/no-array-callback-reference': 'off',
-    'security/detect-non-literal-fs-filename': 'off',
-    'no-undef': 'off',
-    'no-dupe-keys': 'off',
   },
 };
