ef086dbf7811b05db5679ba673954af29ab6828a
jzhzhang66 <59738880+jzhzhang66@users.noreply.github.com>
2021-05-13 17:43:35 -0400

    Text section desktop

--- components/story-slices/text-section/TextSection.js
+++ components/story-slices/text-section/TextSection.js
@@ -1,18 +1,36 @@
-import React from 'react';
 import { renderRichText } from '../../../lib/richText';
 import { TextSectionWrapper } from './TextSection.styles';
+import React, { useState, useRef, useEffect } from 'react';
+import { registerObserver } from '../../../lib/intersectionObserver';
+import { PlaceHolder } from '../../base-components/BaseComponents';
+import { motion } from 'framer-motion';
 
 export default function TextSection({
   vertical_alignment,
   horizontal_alignment,
   text,
 }) {
-  return (
-    <TextSectionWrapper
-      verticalAlignment={vertical_alignment}
-      horizontalAlignment={horizontal_alignment}
-    >
-      {renderRichText(text)}
-    </TextSectionWrapper>
-  );
+  const placeHolderRef = useRef(null);
+  const [visible, setVisible] = useState(false);
+  useEffect(() => {
+    registerObserver(placeHolderRef.current, setVisible);
+  }, []);
+
+  if (visible) {
+    return (
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ type: 'spring', duration: 4, delay: 0.5 }}
+      >
+        <TextSectionWrapper
+          verticalAlignment={vertical_alignment}
+          horizontalAlignment={horizontal_alignment}
+        >
+          {renderRichText(text)}
+        </TextSectionWrapper>
+      </motion.div>
+    );
+  }
+  return <PlaceHolder ref={placeHolderRef} />;
 }
--- style/typography.js
+++ style/typography.js
@@ -56,7 +56,7 @@ const H4 = styled.h3`
   font-size: 1em;
 `;
 
-const P = styled.p`
+const P = styled(motion.p)`
   font-family: ${fonts.poppins};
   font-weight: ${fontWeights.normal};
   font-size: 1em;
