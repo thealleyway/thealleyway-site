7318e6f41aa2a11837441891f53bc994ed196283
jzhzhang66 <59738880+jzhzhang66@users.noreply.github.com>
2021-05-13 12:57:40 -0400

    Made star on newsletter signup bigger

--- components/home-page/newsletter-sign-up/NewsletterSignUp.js
+++ components/home-page/newsletter-sign-up/NewsletterSignUp.js
@@ -106,9 +106,8 @@ export default function NewsletterSignUp({
             <TextInputField
               id="name"
               label="Name"
-              fullWidth
               required
-              biggerStar
+              biggerStar={true}
               showError={errors[fieldNames.NAME]}
               value={fields[fieldNames.NAME] ? fields[fieldNames.NAME] : ''}
               onChange={(e) => {
@@ -119,9 +118,8 @@ export default function NewsletterSignUp({
             <TextInputField
               id="email"
               label="Email"
-              fullWidth
               required
-              biggerStar
+              biggerStar={true}
               showError={errors[fieldNames.EMAIL]}
               value={fields[fieldNames.EMAIL] ? fields[fieldNames.EMAIL] : ''}
               onChange={(e) => setFields({ ...fields, EMAIL: e.target.value })}
