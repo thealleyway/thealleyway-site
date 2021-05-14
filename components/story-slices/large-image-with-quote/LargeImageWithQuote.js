3713e689813b90ff1aa7e56e112157c2f851217f
jzhzhang66 <59738880+jzhzhang66@users.noreply.github.com>
2021-05-13 23:51:31 -0400

    Large image with quote

--- components/story-slices/large-image-with-quote/LargeImageWithQuote.js
+++ components/story-slices/large-image-with-quote/LargeImageWithQuote.js
@@ -1,16 +1,39 @@
-import React from 'react';
 import {
   LargeImage,
   LargeImageWithQuoteWrapper,
   Quote,
 } from './LargeImageWithQuote.styles';
 import { getString } from '../../../lib/richText';
+import React, { useState, useRef, useEffect } from 'react';
+import { registerObserver } from '../../../lib/intersectionObserver';
 
 export default function LargeImageWithQuote({ image, quote }) {
+  const placeHolderRef = useRef(null);
+  const [visible, setVisible] = useState(false);
+
+  useEffect(() => {
+    registerObserver(placeHolderRef.current, setVisible);
+  }, []);
+
   return (
-    <LargeImageWithQuoteWrapper>
-      <LargeImage src={image.url} />
-      <Quote>{getString(quote)}</Quote>
+    <LargeImageWithQuoteWrapper ref={placeHolderRef}>
+      {visible && (
+        <LargeImage
+          src={image.url}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ type: 'spring', duration: 10, delay: 0.2 }}
+        />
+      )}
+      {visible && (
+        <Quote
+          initial={{ opacity: 0, x: 100 }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{ type: 'spring', duration: 4, delay: 0.5 }}
+        >
+          {getString(quote)}
+        </Quote>
+      )}
     </LargeImageWithQuoteWrapper>
   );
 }
