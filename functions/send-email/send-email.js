766cc1fc838d5546e1ec872e77fb2587779e7c91
Jean Zhang <jean.nmn.zhang@gmail.com>
2021-05-14 05:35:32 -0400

    Bringing back Google Scripts

--- functions/send-email/send-email.js
+++ functions/send-email/send-email.js
@@ -1,103 +1,44 @@
 require('dotenv').config();
-
+const fetch = require('node-fetch');
 const process = require('process');
-const { promisify } = require('util');
 
-const sendMailLib = require('sendmail');
+const emailEndpoint = process.env.SEND_EMAIL_API_ENDPOINT;
 
-const { validateEmail, validateLength } = require('./validations');
-
-const sendMail = promisify(sendMailLib());
-
-const NAME_MIN_LENGTH = 1;
-const NAME_MAX_LENGTH = 50;
-const SUBJECT_MIN_LENGTH = 1;
-const SUBJECT_MAX_LENGTH = 50;
-const DETAILS_MIN_LENGTH = 20;
-const DETAILS_MAX_LENGTH = 1e7;
-
-const handler = async (event) => {
-  console.log('1) inside handler', Date.now());
-  if (!process.env.CONTACT_EMAIL) {
+const handler = async function (event) {
+  const { name, subject, email, details } = JSON.parse(event.body);
+  if (!details || !name || !email || !subject) {
     return {
-      statusCode: 500,
-      body: 'process.env.CONTACT_EMAIL must be defined',
-    };
-  }
-  console.log('2)', process.env.CONTACT_EMAIL);
-  const body = JSON.parse(event.body);
-
-  try {
-    validateLength('body.name', body.name, NAME_MIN_LENGTH, NAME_MAX_LENGTH);
-  } catch (error) {
-    return {
-      statusCode: 403,
-      body: error.message,
-    };
-  }
-  console.log('3) name', body.name);
-
-  try {
-    validateEmail('body.email', body.email);
-  } catch (error) {
-    return {
-      statusCode: 403,
-      body: error.message,
+      statusCode: 422,
+      body: 'Name, email, subject, and message are required.',
     };
   }
 
-  console.log('4) email', body.email);
-
   try {
-    validateLength(
-      'body.subject',
-      body.subject,
-      SUBJECT_MIN_LENGTH,
-      SUBJECT_MAX_LENGTH,
+    const response = await fetch(
+      `${emailEndpoint}?name=${name}&email=${email}&subject=${subject}&body=${details}`,
+      {
+        method: 'post',
+        headers: { Accept: 'application/json' },
+      },
     );
-  } catch (error) {
-    return {
-      statusCode: 403,
-      body: error.message,
-    };
-  }
+    if (!response.ok) {
+      // NOT res.status >= 200 && res.status < 300
+      return { statusCode: response.status, body: response.statusText };
+    }
+    const data = await response.json();
 
-  console.log('5) subject', body.subject);
-
-  try {
-    validateLength(
-      'body.details',
-      body.details,
-      DETAILS_MIN_LENGTH,
-      DETAILS_MAX_LENGTH,
-    );
-  } catch (error) {
-    return {
-      statusCode: 403,
-      body: error.message,
-    };
-  }
-
-  console.log('6) details', body.details);
-
-  const descriptor = {
-    from: `"${body.email}" <no-reply@the-alleyway.com>`,
-    to: process.env.CONTACT_EMAIL,
-    subject: body.subject,
-    text: body.details,
-  };
-
-  try {
-    console.log('7) before send mail', descriptor, Date.now());
-    await sendMail(descriptor);
-    console.log('8) after send mail', descriptor, Date.now());
     return {
       statusCode: 200,
-      body: `Email sent successfully from ${body.email}`,
+      body: JSON.stringify({ msg: data.joke }),
     };
   } catch (error) {
-    console.log('9) error', error.message);
-    return { statusCode: 500, body: error.message };
+    // output to netlify function log
+    console.log('error', error);
+    return {
+      statusCode: 500,
+      // Could be a custom message or object i.e. JSON.stringify(err)
+      body: JSON.stringify({ msg: error.message }),
+    };
   }
 };
 
